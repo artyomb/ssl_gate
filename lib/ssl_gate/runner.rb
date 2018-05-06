3e9d091475c170e0b9e018e79b48a3f211dd46d3
Your Name <artem.borodkin@gmail.com>
2018-05-06 19:19:47 +0300

    update

--- lib/ssl_gate/runner.rb
+++ lib/ssl_gate/runner.rb
@@ -2,11 +2,19 @@ require 'yaml'
 
 module SSLGate
   # CLI runner.
-  # Parse options and send command to the correct Controller.
   class Runner
 
+    def self.symbolize_keys(hash)
+      hash.each_with_object({}) do |(key, value), result|
+        new_key = key.is_a?(String) ? key.to_sym : key
+        new_value = value.is_a?(Hash) ? symbolize_keys(value) : value
+        result[new_key] = new_value
+        result
+      end
+    end
+
     def self.start
-      config = YAML.load_file 'SSLGate'
+      config = symbolize_keys YAML.load_file('SSLGate')
 
       EventMachine.run do
         Signal.trap('INT')  { EM.stop if EM.reactor_running? }
